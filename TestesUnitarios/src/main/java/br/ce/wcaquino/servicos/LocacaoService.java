9a89aeb960e9a415ce8f989163a3a1bc5d6273ad
mariel <coutomariel@gmail.com>
2021-04-30 09:25:42 -0300

    Exemplificando a estrutura de um teste

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,34 +1,49 @@
 package br.ce.wcaquino.servicos;
 
-import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-
 import java.util.Date;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 
+import static br.ce.wcaquino.utils.DataUtils.*;
+
 public class LocacaoService {
-	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) {
-		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
-		locacao.setUsuario(usuario);
-		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
 
-		//Entrega no dia seguinte
-		Date dataEntrega = new Date();
-		dataEntrega = adicionarDias(dataEntrega, 1);
-		locacao.setDataRetorno(dataEntrega);
-		
-		//Salvando a locacao...	
-		//TODO adicionar método para salvar
-		
-		return locacao;
-	}
+    public Locacao alugarFilme(Usuario usuario, Filme filme) {
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        locacao.setValor(filme.getPrecoLocacao());
 
-	public static void main(String[] args) {
-		
-	}
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
+
+        return locacao;
+    }
+
+    public static void main(String[] args) {
+        // Cenário
+        Usuario usuario = new Usuario("Mariel");
+        Filme filme = new Filme("O Regresso", 1, 5.00);
+        Date dataEsperada = adicionarDias(new Date(), 2);
+
+        // Execução
+        Locacao locacao = new LocacaoService().alugarFilme(usuario, filme);
+
+        // Validação
+        System.out.println(locacao.getValor() == 5.0);
+        System.out.println(isMesmaData(locacao.getDataLocacao(), new Date()));
+        System.out.println(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)));
+
+
+    }
+
+
 }
